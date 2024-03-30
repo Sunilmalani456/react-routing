260d5af12672ecdeca192ddef448b5a43acd5698
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-03-30 20:54:34 +0530

    add queryString(useSearchParams)

--- src/components/players.jsx
+++ src/components/players.jsx
@@ -1,5 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useLocation, useSearchParams } from "react-router-dom";
+
 const Players = () => {
-  return <div className="container">Players</div>;
+  const location = useLocation(); // mean current location
+  const query = location.search; // mean current search query
+  const [searchParams] = useSearchParams(query); // mean current search params
+
+  const teamId = searchParams.get("teamId");
+  // console.log({ location, searchParams });
+  return <div className="container">Players: {teamId}</div>;
 };
 
 export default Players;
--- src/components/teampage.jsx
+++ src/components/teampage.jsx
@@ -1,9 +1,96 @@
-import { useParams } from "react-router-dom";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { Link, useParams } from "react-router-dom";
+import useTeamNames from "../hooks/useTeamNames";
+import useTeamsArticles from "../hooks/useTeamsArticles";
+import useTeam from "../hooks/useTeam";
+import TeamLogo from "./TeamLogo";
+
+function useTeamPageData(teamId) {
+  const { response: teamNames, loading: LoadingTeamNames } = useTeamNames();
+  const { response: articles, loading: LoadingArticles } =
+    useTeamsArticles(teamId);
+  const { response: team, loading: LoadingTeam } = useTeam(teamId);
+  return {
+    teamNames,
+    articles,
+    team,
+    Loading: LoadingTeamNames || LoadingArticles || LoadingTeam,
+  };
+}
+
+function Deatils({ head, body }) {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="text-2xl sm:text-4xl font-bold">{head}</h2>
+      <p className="text-2xl text-[#f4f73e] sm:text-2xl">{body}</p>
+    </div>
+  );
+}
+
+const slugify = (title) => {
+  // ex : "Hello World?" => "hello-world?"
+  return title.toLowerCase().split(" ").join("-");
+};
 
 const TeamPage = () => {
   const { teamId } = useParams();
+  const { teamNames, articles, team, Loading } = useTeamPageData(teamId);
 
-  return <div>Team id is : {teamId}</div>;
+  // console.log({ teamNames, articles, team, Loading });
+
+  if (Loading === true) {
+    return <p>Loading...</p>;
+  }
+
+  if (!teamNames.includes(teamId)) {
+    return (
+      <h1 className="text-center">This {teamId} does not a valid team.</h1>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center pt-14">
+      <TeamLogo id={teamId} />
+      <h1 className="text-6xl font-bold pt-7">{team.name}</h1>
+      <Link
+        to={{ pathname: "/players", search: `?teamId=${teamId}` }}
+        className="text-xl font-semibold pt-8"
+      >
+        View Roster
+      </Link>
+      <h2 className="text-xl font-semibold pt-8">Championships</h2>
+      <div className="flex flex-wrap justify-center p-3 gap-3 pt-5">
+        {team.championships.map((championship) => (
+          <p className="text-xl" key={championship}>
+            {championship}
+          </p>
+        ))}
+      </div>
+
+      <div className="w-full flex justify-around max-sm:flex-col max-sm:items-center max-sm:gap-8 py-7">
+        <Deatils head="Established" body={team.established} />
+        <Deatils head="Manager" body={team.manager} />
+        <Deatils head="Coach" body={team.coach} />
+        <Deatils head="Record" body={`${team.wins} - ${team.losses}`} />
+      </div>
+
+      <h2 className="text-2xl font-semibold pt-8">Articles</h2>
+      <div className="flex flex-col items-start gap-3 py-5">
+        {articles.map((article) => (
+          <div key={article.id}>
+            <Link
+              to={`/articles/${slugify(article.title)}`}
+              className="text-2xl"
+            >
+              <p>{article.title}</p>
+            </Link>
+            <p>{new Date(article.date).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TeamPage;
