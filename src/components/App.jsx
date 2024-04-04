0cff38df6b0a4bb4a026faf345121c2cb77abfc3
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-04-04 18:30:51 +0530

    add Code Splitting using React.Lazy()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,52 @@
+import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./home";
-import Teams from "./teams";
-import Players from "./players";
 import Navbar from "./navbar";
-import TeamPage from "./teampage";
-import Player from "./player";
-import Team from "./team";
-import Article from "./article";
-import Articles from "./articles";
+import Loading from "./loading";
+const Home = React.lazy(() => import("./home"));
+const Teams = React.lazy(() => import("./teams"));
+const Players = React.lazy(() => import("./players"));
+const TeamPage = React.lazy(() => import("./teampage"));
+const Player = React.lazy(() => import("./player"));
+const Team = React.lazy(() => import("./team"));
+const Article = React.lazy(() => import("./article"));
+const Articles = React.lazy(() => import("./articles"));
 
 const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/players" element={<Players />}>
-            <Route path=":playerId" element={<Player />} />
-            <Route
-              path=""
-              element={
-                <h className="flex h-full text-xl font-bold justify-center items-center">
-                  Select a Player
-                </h>
-              }
-            />
-          </Route>
-          <Route path="/teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route
-              path=""
-              element={
-                <h className="w-full flex text-xl font-bold justify-center items-center">
-                  Select a Team
-                </h>
-              }
-            />
-          </Route>
-          <Route path="/:teamId" element={<TeamPage />} />
-          <Route path="/:teamId/articles" element={<Articles />}>
-            <Route path=":articleId" element={<Article />} />
-          </Route>
-        </Routes>
+        <React.Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/players" element={<Players />}>
+              <Route path=":playerId" element={<Player />} />
+              <Route
+                path=""
+                element={
+                  <h className="flex h-full text-xl font-bold justify-center items-center">
+                    Select a Player
+                  </h>
+                }
+              />
+            </Route>
+            <Route path="/teams" element={<Teams />}>
+              <Route path=":teamId" element={<Team />} />
+              <Route
+                path=""
+                element={
+                  <h className="w-full flex text-xl font-bold justify-center items-center">
+                    Select a Team
+                  </h>
+                }
+              />
+            </Route>
+            <Route path="/:teamId" element={<TeamPage />} />
+            <Route path="/:teamId/articles" element={<Articles />}>
+              <Route path=":articleId" element={<Article />} />
+            </Route>
+          </Routes>
+        </React.Suspense>
       </div>
     </Router>
   );
